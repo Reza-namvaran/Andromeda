00d3a7f284779624ac5d59798051b77e706b9a0f
Reza-namvaran <reza.namvaran823@gmail.com>
2025-09-17 02:13:44 +0330

    (feat): Add shared key value database between client threads

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,22 @@ mod commands;
 
 use resp::parse_resp;
 use commands::handle_command;
+use std::collections::HashMap;
+use std::sync::{Arc, Mutex};
 use std::io::{BufReader, Write};
 use std::net::{TcpListener, TcpStream};
 use std::thread;
 
-fn handle_client(stream: TcpStream) {
+type DB = Arc<Mutex<HashMap<String, String>>>;
+
+fn handle_client(stream: TcpStream, db: DB) {
     let mut reader = BufReader::new(stream.try_clone().unwrap());
     let mut writer = stream;
 
     loop {
         match parse_resp(&mut reader) {
             Ok(resp) => {
-                if let Some(reply) = handle_command(resp) {
+                if let Some(reply) = handle_command(resp, &db) {
                     if writer.write_all(reply.as_bytes()).is_err() {
                         break;
                     }
@@ -26,13 +30,16 @@ fn handle_client(stream: TcpStream) {
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:6379")?;
     println!("Server listening on port 6379...");
+
+    let db: DB = Arc::new(Mutex::new(HashMap::new()));
 
     for stream in listener.incoming() {
         match stream {
             Ok(stream) => {
-                thread::spawn(|| handle_client(stream));
+                let db = Arc::clone(&db);
+                thread::spawn(|| handle_client(stream, db));
             }
             Err(e) => eprintln!("Connection failed: {}", e),
         }
